92ef17522ce54d1fb7e7ead67d23604b4f96cca5
zhaoxin <83268606@qq.com>
2024-09-11 21:22:39 +0800

    add folder 'play'

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,16 @@ function resolve(path) {
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
+    root: command === 'serve' ? 'play' : '.',
     define: {
       __DEV__: command === 'serve',
     },
     resolve: {
       alias: {
-        vdom: resolve('./src/vdom'),
-        core: resolve('./src/vdom/core'),
-        dom: resolve('./src/vdom/dom'),
-        shared: resolve('./src/vdom/shared'),
+        vdom: resolve('./src'),
+        core: resolve('./src/core'),
+        dom: resolve('./src/dom'),
+        shared: resolve('./src/shared'),
       },
     },
   };
